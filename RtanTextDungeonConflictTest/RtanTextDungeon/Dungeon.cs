eb58db2c3fc3aa82dd1ff4cd6790c83583242ed4
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-01-11 14:18:32 +0900

    Update Dungeon.cs

--- RtanTextDungeonConflictTest/RtanTextDungeon/Dungeon.cs
+++ RtanTextDungeonConflictTest/RtanTextDungeon/Dungeon.cs
@@ -564,6 +564,7 @@ namespace RtanTextDungeon
             {
                 Console.WriteLine(" __               ");
                 Console.WriteLine("|__|.-----..-----.");
+                Console.WriteLine("|__|.-----..-----.");
                 Console.WriteLine("|  ||     ||     |");
                 Console.WriteLine("|__||__|__||__|__|");
 
